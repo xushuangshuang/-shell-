4642f58ccc768013335c09058de001a947e70613
Baldur <master@baldurtech.com>
2014-08-06 14:42:41 +0800

    添加了 ContactRepository 的测试

--- src/test/java/AppTest.java
+++
@@ -1,7 +0,0 @@
-import com.baldurtech.unit.MiniatureSpiceTestCase;
-
-public class AppTest extends MiniatureSpiceTestCase {
-    public void test_fail() {
-        assertEquals("foo", "bar");
-    }
-}
---
+++ src/test/java/com/baldurtech/turnt/octo/adventure/ContactRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.baldurtech.turnt.octo.adventure;
+
+import com.baldurtech.unit.MiniatureSpiceTestCase;
+
+public class ContactRepositoryTest extends MiniatureSpiceTestCase {
+
+}
