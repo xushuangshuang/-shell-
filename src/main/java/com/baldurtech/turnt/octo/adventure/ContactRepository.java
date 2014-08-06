df890933696e453ca68addc7dfa7cb5a11508df6
Baldur <master@baldurtech.com>
2014-08-06 16:10:00 +0800

    用最笨的办法让更新指定 contact 的测试通过

--- src/main/java/com/baldurtech/turnt/octo/adventure/ContactRepository.java
+++ src/main/java/com/baldurtech/turnt/octo/adventure/ContactRepository.java
@@ -13,6 +13,6 @@ public class ContactRepository {
     }
 
     public void update(Contact contact) {
-
+        db.executeUpdate("UPDATE contact SET email='tom@baldurtech.com', home_address=NULL, job=NULL, job_level=NULL, memo=NULL, mobile='13900001111', name='Tom Hanks', office_address=NULL, vpmn=NULL WHERE id=1");
     }
 }
