cf02fe801869b1a730fe6699a5efb876c6d8f0ca
Baldur <master@baldurtech.com>
2014-08-11 17:01:54 +0800

    增加了接口 ActionContext 及其实现

---
+++ src/main/java/com/baldurtech/turnt/octo/adventure/ActionContext.java
@@ -0,0 +1,9 @@
+package com.baldurtech.turnt.octo.adventure;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ActionContext {
+
+}
---
+++ src/main/java/com/baldurtech/turnt/octo/adventure/ActionContextImpl.java
@@ -0,0 +1,18 @@
+package com.baldurtech.turnt.octo.adventure;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ActionContextImpl implements ActionContext {
+
+    final ServletContext servletContext;
+    final HttpServletRequest request;
+    final HttpServletResponse response;
+
+    public ActionContextImpl(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response) {
+        this.servletContext = servletContext;
+        this.request = request;
+        this.response = response;
+    }
+}
