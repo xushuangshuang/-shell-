3c319f6b528f0528bc0207d104565bb5db1a1349
Baldur <master@baldurtech.com>
2014-08-11 11:22:40 +0800

    添加测试 /contact 也应该由 ContactAction 来处理

--- src/test/java/com/baldurtech/turnt/octo/adventure/DispatchServletTest.java
+++ src/test/java/com/baldurtech/turnt/octo/adventure/DispatchServletTest.java
@@ -4,10 +4,17 @@ import com.baldurtech.unit.MiniatureSpiceTestCase;
 
 public class DispatchServletTest extends MiniatureSpiceTestCase {
 
-    public void test_uri_contat_show_应该由ContactAction来处理() {
+    public void test_uri_contact_show_应该由ContactAction来处理() {
         DispatchServlet dispatchServlet = new DispatchServlet();
 
         assertEquals("com.baldurtech.turnt.octo.adventure.ContactAction"
                      , dispatchServlet.getActionClassNameByUri("/contact/show"));
+    }
+
+    public void test_uri_contact_应该由ContactAction来处理() {
+        DispatchServlet dispatchServlet = new DispatchServlet();
+
+        assertEquals("com.baldurtech.turnt.octo.adventure.ContactAction"
+                     , dispatchServlet.getActionClassNameByUri("/contact"));
     }
 }
