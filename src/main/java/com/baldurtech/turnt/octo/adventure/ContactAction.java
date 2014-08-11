6de6c6ec05dcae6f7aeef9aed812ddfeb907fd05
Baldur <master@baldurtech.com>
2014-08-11 19:06:25 +0800

    实现了 contact 的 show 方法

--- src/main/java/com/baldurtech/turnt/octo/adventure/ContactAction.java
+++ src/main/java/com/baldurtech/turnt/octo/adventure/ContactAction.java
@@ -1,5 +1,8 @@
 package com.baldurtech.turnt.octo.adventure;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class ContactAction extends Action {
     public ContactAction(ActionContext actionContext) {
         super(actionContext);
@@ -7,5 +10,9 @@ public class ContactAction extends Action {
 
     public void index() {
         println("Hi, contact!");
+    }
+
+    public Map<String, Object> show() {
+        return new HashMap<String, Object>();
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/contact/show.jsp
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Show Contact</title>
+  </head>
+  <body>
+    <h1>Show Contact</h1>
+  </body>
+</html>
