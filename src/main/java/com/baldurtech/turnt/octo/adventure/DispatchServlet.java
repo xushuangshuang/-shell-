e8b6e600550dd590c1a64ed8351472e2d2a1216b
Baldur <master@baldurtech.com>
2014-08-11 17:03:32 +0800

    实现了 DispatchServlet 的 service 方法

--- src/main/java/com/baldurtech/turnt/octo/adventure/DispatchServlet.java
+++ src/main/java/com/baldurtech/turnt/octo/adventure/DispatchServlet.java
@@ -1,20 +1,39 @@
 package com.baldurtech.turnt.octo.adventure;
 
 import java.io.IOException;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class DispatchServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void service(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-
+        try {
+            String uri = request.getRequestURI();
+            ActionContext actionContext = new ActionContextImpl(getServletContext(), request, response);
+            Class actionClass = getActionByUri(uri);
+            @SuppressWarnings("unchecked")
+            Constructor actionConstructor = actionClass.getDeclaredConstructor(ActionContext.class);
+            Action actionInstance = (Action) actionConstructor.newInstance(actionContext);
+            @SuppressWarnings("unchecked")
+            Method method = actionClass.getDeclaredMethod(getMethodNameByUri(uri));
+            method.invoke(actionInstance);
+        } catch(Exception e) {
+        }
     }
 
     public String defaultPackageName = "com.baldurtech.turnt.octo.adventure";
     public String defaultSuffix = ".jsp";
+
+    public Class getActionByUri(String uri) throws Exception {
+        return Class.forName(getActionClassNameByUri(uri));
+    }
 
     public String getActionClassNameByUri(String uri) {
         String[] uriParts = splitBySlash(uri);
@@ -30,10 +49,6 @@ public class DispatchServlet extends HttpServlet {
             return "index";
         }
         return removeDefaultSuffix(uriParts[indexOfMethodName]);
-    }
-
-    public String getViewPage(String uri) {
-        return "/WEB-INF/jsp" + uri;
     }
 
     public String[] splitBySlash(String uri) {
