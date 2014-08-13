a268ec80cbff6e3d7be878ce3c35fecbf03a2219
Baldur <master@baldurtech.com>
2014-08-13 14:47:29 +0800

    添加失败的测试：跳转到带参数的 action uri

--- src/test/java/com/baldurtech/turnt/octo/adventure/ActionContextImplTest.java
+++ src/test/java/com/baldurtech/turnt/octo/adventure/ActionContextImplTest.java
@@ -9,4 +9,8 @@ public class ActionContextImplTest extends MiniatureSpiceTestCase {
     public void test_toRealUri_不带参数的actionUri() {
         assertEquals("contact/list.do", actionContext.toRealUri("contact/list"));
     }
+
+    public void test_toRealUri_contact_show带参数的actionUri() {
+        assertEquals("contact/show.do?id=1", actionContext.toRealUri("contact/show?id=1"));
+    }
 }
